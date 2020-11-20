b680f16b2fbe6b98b25872315d2738ffdb3d02ef
nildembay <nildembay@gmail.com>
2020-11-20 18:04:35 +0300

    mesaj

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,11 @@
+import static java.lang.System.out;
+
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("nilufer");
-        System.out.println("demir");
+        out.println("lifer");
+        out.println("demir");
+        out.println("feature");
+        out.println("master");
+        out.println("feature");
     }
 }
